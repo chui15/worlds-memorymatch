71ede34e22bc02fc02e8321917a6068bf6305be4
Christie Hui <cwsh15@gmail.com>
2019-08-22 14:10:31 -0700

    Fixed card match

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,38 @@
+$(document).ready(initializeApp);
 
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = null;
+var firstCardClickedImage = null;
+var secondCardClickedImage = null;
+
+function initializeApp(){
+  $('.lfz-card').on('click', handleCardClick);
+}
+
+function handleCardClick( event ) {
+  console.log(event);
+  $(event.currentTarget).toggleClass('hidden', true);
+  if (!firstCardClicked) {
+    firstCardClicked = $(event.currentTarget);
+  } else {
+    secondCardClicked = $(event.currentTarget);
+    firstCardClickedImage = $(firstCardClicked).siblings()[0];
+    secondCardClickedImage = $(secondCardClicked).siblings()[0];
+    console.log(firstCardClickedImage);
+    console.log(secondCardClickedImage);
+    if ($(firstCardClickedImage).attr('class') === $(secondCardClickedImage).attr('class')) {
+      console.log('cards match');
+      matches +=1;
+      firstCardClicked = null;
+      secondCardClicked = null;
+    } else {
+      setTimeout(function () {
+        $(firstCardClicked).toggleClass('hidden', false);
+        $(secondCardClicked).toggleClass('hidden', false);
+        firstCardClicked = null;
+        secondCardClicked = null;
+      }, 1500);
+    }
+  }
+}
