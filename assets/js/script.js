ad68ac82772d35ea0bb89b05d25d6a3e5c3ff246
Christie Hui <cwsh15@gmail.com>
2019-08-23 17:20:34 -0700

    added a lot more CSS styling, audio, and another theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,35 @@ var max_matches = 9;
 var attempts = 0;
 var games_played = 0;
 var accuracy = 0;
-var veryFirstClick = false;
+var images_array = ['card1', 'card2', 'card3', 'card4', 'card5', 'card6', 'card7', 'card8', 'card9'];
+//1) shuffle, 2) concat/double, 3)re-shuffle, 4) jQuery new box 5) for loop to assign to new boxes
+
+function firstChoice(){
+  var modal1 = document.getElementById('modal1');
+  $(modal1).show();
+  $('.choice1').on('click', function () {
+    $(modal1).hide();
+    var audio = document.getElementsByTagName('audio')[0];
+    audio.play();
+  });
+  $('.choice2').on('click', function(){
+    $('body').addClass('theme2');
+    $('.back').toggleClass('back-2');
+    $(modal1).hide();
+    var audio2 = document.getElementsByTagName('audio')[1];
+    audio2.play();
+  });
+}
 
 function initializeApp(){
+  clickHandlers();
+}
+
+function clickHandlers(){
+  $('.close2').on('click', function () {
+    $(modal2).hide();
+    resetStats();
+  });
   $('.back').on('click', handleCardClick);
 }
 
@@ -20,7 +46,6 @@ function handleCardClick( event ) {
   $(event.currentTarget).toggleClass('hidden', true);
   if (!firstCardClicked) {
     firstCardClicked = $(event.currentTarget);
-    gameCount();
   } else {
     secondCardClicked = $(event.currentTarget);
     firstCardClickedImage = $(firstCardClicked).siblings()[0];
@@ -32,16 +57,9 @@ function handleCardClick( event ) {
       attempts += 1;
       calculateAccuracy();
       $('.stats-bar-section:nth-child(5)').text(attempts);
-      if (matches == max_matches) {
-        var modal = document.getElementById('modal');
-        $(modal).show();
-        var span = document.getElementsByClassName('close')[0];
-        $(span).on('click', function(){
-          $(modal).hide();
-          alert('before calling reset');
-          resetStats();
-          alert('after calling reset');
-        });
+      if (matches === max_matches) {
+        var modal2 = document.getElementById('modal2');
+        $(modal2).show();
       }
       firstCardClicked = null;
       secondCardClicked = null;
@@ -66,27 +84,31 @@ function calculateAccuracy(){
   $('.stats-bar-section:nth-child(7)').text(accuracy);
 }
 
-function gameCount(){
-  if (veryFirstClick == false) {
-    veryFirstClick = true;
-    games_played += 1;
-    $('.stats-bar-section:nth-child(3)').text(games_played);
-  }
-}
-
 function resetStats(){
   matches = 0;
   attempts = 0;
   accuracy = 0;
-  //veryFirstClick = false;
-  alert('before' + games_played);
-  gameCount();
-  alert('after' + games_played);
+  games_played+=1;
+  $('.stats-bar-section:nth-child(3)').text(games_played);
   $('.stats-bar-section:nth-child(5)').text(attempts);
   $('.stats-bar-section:nth-child(7)').text(accuracy);
   flipAllCardsBack();
+  shuffleDeck(images_array);
 }
 
 function flipAllCardsBack(){
   $('.back').toggleClass('hidden', false);
 }
+
+// function shuffleDeck(array){
+//   var randomNumber;
+//   var arrayLength = images_array.length-1;
+//   var spotHolder;
+//   for (var i = 0; i < arrayLength; i++){
+//     randomNumber = Math.floor(Math.random() * arrayLength);
+//     spotHolder = array[i];
+//     array[i] = array[randomNumber];
+//     array[randomNumber] = spotHolder;
+//   }
+//   return array;
+// }
